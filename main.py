b37a630f615156be3622c19b8259cb90c5ab4ac2
29-29 <ackx29sodo@gmail.com>
2025-05-28 03:46:43 +0800

    increasing slots data gathering

--- main.py
+++ main.py
@@ -10,14 +10,15 @@ merge 				= ['g','h']
 sort 					= ['s','t']
 thread_counts = [1, 2, 4]
 hostfiles 		= ['hostfile_all','hostfile_servers']
+slots					= [i for i in range(1, 32)]
 
-def mpi(N, args, hostfile)->float:
-	mpirun = ['mpirun','--mca','btl_tcp_if_include','br0','--hostfile',hostfile,'python3','src/main.py']
+def mpi(N, args, hostfile, slots=None)->float:
+	mpirun = ['mpirun','--mca','btl_tcp_if_include','br0','--hostfile',hostfile,'--np' if slots else '',slots if slots else '','python3','src/main.py']
 	time = subprocess.run([*mpirun,*args,N], stdout=subprocess.PIPE)
 	time = time.stdout.decode().strip() or 0
 	return float(time)
 
-if __name__ == '__main__':
+def incN():
 	times = []
 	for hostfile in hostfiles:
 		print(hostfile)
@@ -56,3 +57,38 @@ if __name__ == '__main__':
 	df = pd.DataFrame(times)
 	os.makedirs('results',exist_ok=True)
 	df.to_csv('results/runtimes.csv',index=False)
+
+def incSlots():
+	times = []
+	for n in slots:
+		print(n)
+		for m_method in merge:
+			print('\t%s'%('gather' if m_method == 'g' else 'hierarchical'))
+			for s_method in sort:
+				print('\t\t%s'%('threaded' if s_method == 't' else 'sequential'))
+				if s_method == 't':
+					for threads in thread_counts:
+						time = mpi(str(Ns[-1]),[modes[1],m_method,s_method,threads],'hostfile_all',slots=n)
+						times.append({
+							'N':Ns[-1],'Mode':'parallel',
+							'MergeMethod':('gather' if m_method == 'g' else 'hierarchical'),
+							'SortMethod':'threaded','Threads':threads, 'Slots':n,
+							'Time':time
+						})
+				else:
+					time = mpi(str(Ns[-1]),[modes[1],m_method,s_method],'hostfile_all',slots=n)
+					times.append({
+						'N':Ns[-1],'Mode':'parallel',
+						'MergeMethod':('gather' if m_method == 'g' else 'hierarchical'),
+						'SortMethod':'sequential','Slots':n,
+						'Time':time
+					})
+
+	df = pd.DataFrame(times)
+	os.makedirs('results',exist_ok=True)
+	df.to_csv('results/incSlots.csv',index=False)
+
+if __name__ == '__main__':
+	# incN()
+	incSlots()
+	pass
